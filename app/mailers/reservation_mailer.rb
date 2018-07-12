a90481411dcae788c918e3432f05914537ea13d0
Koh Jian Jia <kohjianjia@McApple.local>
2018-07-12 16:42:54 +0800

    mailing to host added

---
+++ app/mailers/reservation_mailer.rb
@@ -0,0 +1,18 @@
+# mailer controller
+class ReservationMailer < ApplicationMailer
+
+	default from: 'jjkohtest@gmail.com'
+
+	def booking_email(customer, host, reservation_id)
+		@customer = customer
+		@host = host
+		@reservation_id = reservation_id
+		@url  = "http://localhost:3000/listings/#{@host.id}/reservations/#{@reservation_id}"
+		email_with_name = %("#{@host.user.name}" <#{@host.user.email}>)
+		mail(to: 'jjkohtest@gmail.com', subject: 'Yay! You\'ve got a booking.')
+	end
+
+end
+
+
+
