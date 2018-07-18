2f854f837baa695a369bb72782b0863be35a150f
Koh Jian Jia <kohjianjiajj@gmail.com>
2018-07-18 11:12:34 +0800

    Procfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+` 1qRails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -91,4 +91,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            ENV['EMAIL'],
+  password:             ENV['PASSWORD'],
+  authentication:       'login',
+  enable_starttls_auto: true  }
+  
 end
